f232fc8d3d7877a55f3dc9d09e138630208dc09f
RYAN <RTIPPER44@GMAIL.COM>
2020-10-08 20:28:15 -0700

    added functions for lab 4

--- js/app.js
+++ js/app.js
@@ -15,110 +15,131 @@ var score = 0;
 
 // Question 1
 var local = prompt('Do you think I am originally from Washington?').toLowerCase();
-if (local === 'yes' || local === 'y ') {
-    // console.log('No, I\'m actually from Cali');
-    alert('No, I\'m actually from California.')
-} else if (local === 'no' || local ==='n') {
-    alert('You are correct! I am from California.');
-    score++;
+function questionOne() {
+    if (local === 'yes' || local === 'y ') {
+        // console.log('No, I\'m actually from Cali');
+        alert('No, I\'m actually from California.')
+    } else if (local === 'no' || local ==='n') {
+        alert('You are correct! I am from California.');
+        score++;
+    }
 }
+questionOne();
 
 // Question 2
 var hobbie = prompt('Do I like painting?').toLowerCase();
-if (hobbie === 'yes' || hobbie === 'y') {
-    // console.log('No, I actually LOVE painting');
-    alert('No because, I actually L.O.V.E. painting!!!')
-} else if (hobbie === 'no' || 'n') {
-    alert('You are correct, because I LOVE to paint!!!');
-    score++;
+function questionTwo() {
+    if (hobbie === 'yes' || hobbie === 'y') {
+        // console.log('No, I actually LOVE painting');
+        alert('No because, I actually L.O.V.E. painting!!!')
+    } else if (hobbie === 'no' || 'n') {
+        alert('You are correct, because I LOVE to paint!!!');
+        score++;
+    }
 }
+questionTwo();
 
 // Question 3
 var pets = prompt('Do I have a cat?').toLowerCase
     ()
-if (pets === 'yes' || pets === 'y') {
-    // console.log('No, I actually have a dog name Sherman');
-    alert('No, I actually have a dog and his name is Sherman.')
-} else if (pets === 'no' || 'n') {
-    alert('You are correct.  I\'m allergic to cats, I have a dog name Sherman.');
-    score++;
+function questionThree() {    
+    if (pets === 'yes' || pets === 'y') {
+        // console.log('No, I actually have a dog name Sherman');
+        alert('No, I actually have a dog and his name is Sherman.')
+    } else if (pets === 'no' || 'n') {
+        alert('You are correct.  I\'m allergic to cats, I have a dog name Sherman.');
+        score++;
+    }
 }
+questionThree();
 
 // Question 4
 var kids = prompt('Do I have kids?').toLowerCase
     ()
-if (kids === 'yes' || kids === 'y') {
-    // console.log('Yes, I have 2');
-    alert('Yes, I have 2, my daughter Sydney and my son named Trey')
-} else if (kids === 'no' || 'n') {
-    alert('You are correct because, they aren\'t kids, they are actually young adults. ');
-    score++;
+function questionFour() {    
+    if (kids === 'yes' || kids === 'y') {
+        // console.log('Yes, I have 2');
+        alert('Yes, I have 2, my daughter Sydney and my son named Trey')
+    } else if (kids === 'no' || 'n') {
+        alert('You are correct because, they aren\'t kids, they are actually young adults. ');
+        score++;
+    }
 }
+questionFour();
 
 // Question 5
 var married = prompt('Am I married?').toLowerCase
     ()
-if (married === 'yes' || married === 'y') {
-    // console.log('Yes and his name is Jack.  HA! Jacq & Jack');
-    alert('Yes, and my husband\'s name is Jack.  HA! Jack & Jacque!');
-    score++;
-} else if (married === 'no' || 'n') {
-    alert('I actually have been married for 25 years.')
+function questionFive() {    
+    if (married === 'yes' || married === 'y') {
+        // console.log('Yes and his name is Jack.  HA! Jacq & Jack');
+        alert('Yes, and my husband\'s name is Jack.  HA! Jack & Jacque!');
+        score++;
+    } else if (married === 'no' || 'n') {
+        alert('I actually have been married for 25 years.')
+    }
 }
+questionFive();
 
 // Question 6
 
 var tries = 0;
 var shermanAge = 8;
+function questionSix() {
 
-while (tries < 3 ) {
-    var guessGame = parseInt(prompt('Can you guess the age of my dog Sherman?'));
-    
+    while (tries < 3 ) {
+        var guessGame = parseInt(prompt('Can you guess the age of my dog Sherman?'));
+        
 
-    // If user guesses correctly 
-    if (guessGame === shermanAge) {
-        alert(`You are correct ${visitorName}!`);
-        score++;
-        break;
-    // If user guesses too high
-    } else if (guessGame > shermanAge) {
-        alert(`Well, not quite ${visitorName}, that guess is too high!`);
-        tries++
-    // If user guesses too low    
-    } else if (guessGame < 8) {
-        alert(`Sorry ${visitorName}, that is too low`);
-        tries++
-    // catch all 
-    } else {
-        alert('invalid response');
-    } 
+        // If user guesses correctly 
+        if (guessGame === shermanAge) {
+            alert(`You are correct ${visitorName}!`);
+            score++;
+            break;
+        // If user guesses too high
+        } else if (guessGame > shermanAge) {
+            alert(`Well, not quite ${visitorName}, that guess is too high!`);
+            tries++
+        // If user guesses too low    
+        } else if (guessGame < 8) {
+            alert(`Sorry ${visitorName}, that is too low`);
+            tries++
+        // catch all 
+        } else {
+            alert('invalid response');
+        } 
 
-    if (tries === 3 && guessGame !== shermanAge) {
-        alert("Sorry, you lose. The correct answer is 8.")
+        if (tries === 3 && guessGame !== shermanAge) {
+            alert("Sorry, you lose. The correct answer is 8.")
+        }
     }
-}     
+}
+questionSix();     
 
 var guesses = 6;
 var guessesCorrect = false;
 var rightAnswer = ['apples', 'pineapple', 'kiwi', 'dragonfruit', 'mango'];
 var guessFav = prompt('Can you guess my favorite fruit?');
+function questionSeven() {
 
-while (guesses > 0 && !guessesCorrect) {
-    guesses--;
+    while (guesses > 0 && !guessesCorrect) {
+        guesses--;
 
-    for (var i = 0; i < rightAnswer.length; i++) {
-        if (rightAnswer[i] === guessFav) {
-            guessesCorrect = true;
-            alert(`That is correct, I like ${guessFav}`);
-            score++;
+        for (var i = 0; i < rightAnswer.length; i++) {
+            if (rightAnswer[i] === guessFav) {
+                guessesCorrect = true;
+                alert(`That is correct, I like ${guessFav}`);
+                score++;
+            }
+        }
+        if (guesses > 0 && !guessesCorrect) {
+            guessFav = prompt(`No, I don't like ${guessFav}, guess again.`)
+        }
+        if (guesses === 0 && !guessesCorrect) {
+            alert(`Sorry, you lose, the correct answers are: ${rightAnswer}`);
         }
     }
-    if (guesses > 0 && !guessesCorrect) {
-        guessFav = prompt(`No, I don't like ${guessFav}, guess again.`)
-    }
-    if (guesses === 0 && !guessesCorrect) {
-        alert(`Sorry, you lose, the correct answers are: ${rightAnswer}`);
-    }
 }
+questionSeven();
 
 alert(`you scored ${score}/7`)
